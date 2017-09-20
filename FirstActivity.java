213a2bfd43171bd93faf3dce642afe228dc3a802
Henry Huang <henry@Henrys-Air.home>
2017-09-20 04:15:03 -0400

    Added some comments in the code

--- FirstActivity.java
+++ FirstActivity.java
@@ -9,14 +9,14 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     public Button cameraButton;
-    // Allows the user to press the camera button to go to camera activity
+    // Allows the user to press the camera button to use the camera
     public void pressCameraButton(){
         cameraButton = (Button)findViewById(R.id.cameraButton);
         cameraButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // MainActivity is the first activity when app is opened
-                // CameraActivity is opens the camera
+                // CameraActivity is the button that changes to the camera
                 Intent camButton = new Intent(MainActivity.this, CameraActivity.class);
                 startActivity(camButton);
             }
