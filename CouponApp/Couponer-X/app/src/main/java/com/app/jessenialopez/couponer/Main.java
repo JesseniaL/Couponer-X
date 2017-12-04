5035e61b93448055d926c29e2c5d628411423815
mfabian0302 <marcos.fabian86@myhunter.cuny.edu>
2017-12-04 11:47:57 -0500

    Combined
    opened Henrry's code with Jessenia's code

--- CouponApp/Couponer-X/app/src/main/java/com/app/jessenialopez/couponer/Main.java
+++ CouponApp/Couponer-X/app/src/main/java/com/app/jessenialopez/couponer/Main.java
@@ -74,15 +74,8 @@ public class Main extends AppCompatActivity {
             //m.str(storeName);
 
             public void onClick(View v) {
-//                Toast.makeText(Main.this, "Im In" , Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(Main.this, ViewStores.class);
-                if (intent != null)
-                    Toast.makeText(Main.this, "Im In" , Toast.LENGTH_SHORT).show();
-                else
-                    Toast.makeText(Main.this, "Im out" , Toast.LENGTH_SHORT).show();
-
-//
-//                startActivity(intent);
+                Intent intent = new Intent(Main.this, MainActivity.class);
+                startActivity(intent);
             }
             /*
             @Override
